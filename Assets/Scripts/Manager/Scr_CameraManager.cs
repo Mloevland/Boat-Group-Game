d5f907ad39e1fc2b8d6feea8a6dbb68a85c23c3f
Mathias2139 <mathias.algot@sybogames.com>
2025-04-29 15:03:14 +0200

    Updated Shader to fit multiple resolutions

--- Assets/Scripts/Manager/Scr_CameraManager.cs
+++ Assets/Scripts/Manager/Scr_CameraManager.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class Scr_CameraManager : MonoBehaviour
 {
     private GameObject currentCamera;
+    public RenderTexture criticalObjectTexture;
+
+    private void Awake()
+    {
+        criticalObjectTexture.height = Mathf.RoundToInt(Screen.height * 0.5f);
+        criticalObjectTexture.width = Mathf.RoundToInt(Screen.width * 0.5f);
+
+    }
+
     public void UpdateCamera(GameObject camera)
     {
         camera.SetActive(true);
