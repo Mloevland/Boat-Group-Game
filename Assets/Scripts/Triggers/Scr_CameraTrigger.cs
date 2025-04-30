50a8a18dbad7040ef4e216719f768cd8923ea92a
Mathias2139 <mathias.algot@sybogames.com>
2025-04-30 11:44:59 +0200

    Camera Collider renderer

--- Assets/Scripts/Triggers/Scr_CameraTrigger.cs
+++ Assets/Scripts/Triggers/Scr_CameraTrigger.cs
@@ -15,4 +15,18 @@ public class Scr_CameraTrigger : MonoBehaviour
             Debug.Log("Camera Triggered");
         }
     }
+
+    void OnDrawGizmos()
+    {
+
+
+        // Convert the local coordinate values into world
+        // coordinates for the matrix transformation.
+        Gizmos.matrix = transform.localToWorldMatrix;
+
+        Gizmos.color = new Color(0.75f, 0.3f, 0.0f, 0.25f);
+        Gizmos.DrawCube(Vector3.zero, Vector3.one);
+        Gizmos.color = new Color(0.75f, 0.1f, 0.0f, 0.6f);
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
 }
