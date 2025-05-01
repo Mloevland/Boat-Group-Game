f517b0088d61a94eaf4da2720aa34a316face720
Mathias2139 <mathias.algot@sybogames.com>
2025-05-01 11:45:41 +0200

    Player Movement Changes

--- Assets/Scripts/Character/Scr_CharacterMovement.cs
+++ Assets/Scripts/Character/Scr_CharacterMovement.cs
@@ -4,7 +4,9 @@ public class Scr_CharacterMovement : MonoBehaviour
 {
     public Animator ani;
     public Rigidbody rb;
+    public Transform positionReferencePoint;
     public float moveSpeed;
+    public LayerMask groundLayer;
 
     [Header("Movement Settings")]
     public float acceleration = 10f;
@@ -12,8 +14,14 @@ public class Scr_CharacterMovement : MonoBehaviour
     public float velPower = 2f;
     public float jumpForce = 4f;
 
+    private float jumpResetBuffer = 0.2f;
+    private float jumpResetTimer = 0;
+    private bool bufferJumpReset = false;
+
+    private bool overrideMovement;
+
     //Movement Variables
-    private bool isGrounded = false;
+    private bool isGrounded = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,32 +29,104 @@ public class Scr_CharacterMovement : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, 1.05f))
+        CheckGrounded();
+
+        if (!isGrounded)
         {
-            isGrounded = true;
+            RaycastHit hit;
+            if(Physics.Raycast(positionReferencePoint.position,transform.forward, out hit, 0.6f, groundLayer))
+            {
+                PlayOverrideAnimation("Climb");
+            }
+            else
+            {
+                Debug.DrawLine(positionReferencePoint.position, positionReferencePoint.position + transform.forward*0.6f, Color.red);
+            }
+            
+        }
+
+    }
+
+    public void PlayOverrideAnimation(string animationName)
+    {
+
+    }
+
+    private void CheckGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(positionReferencePoint.position,0.8f, Vector3.down, out hit, 0.22f, groundLayer))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.green);
+            if (isGrounded)
+                return;
+            if (bufferJumpReset)
+                return;
+
+            SetGrounded(true);
         }
         else
         {
-            //isGrounded = false;
+            SetGrounded(false);
         }
+    }
+
+    private void Update()
+    {
+        HandleJumpBuffers();
+    }
+
+    private void HandleJumpBuffers()
+    {
+        if (bufferJumpReset)
+        {
+            jumpResetTimer -= Time.deltaTime;
+            if (jumpResetTimer <= 0)
+            {
+                bufferJumpReset = false;
+            }
+        }
+    }
+
+    private void SetGrounded(bool value)
+    {
+        isGrounded = value;
+        ani.SetBool("isGrounded", value);
     }
 
     public void MoveCharacter(Vector2 direction)
     {
         direction = Vector2.ClampMagnitude(direction, 1);
-        ani.SetFloat("Speed", direction.magnitude);
-        Vector2 movement = CalculateMovementForce(direction, moveSpeed);
-        rb.AddForce(new Vector3(movement.x, 0, movement.y) * Time.deltaTime);
-        //transform.position = transform.position + new Vector3(direction.x,0,direction.y) * Time.deltaTime * moveSpeed;
-        if(direction != Vector2.zero)
+
+        if (isGrounded)
         {
-            Quaternion desiredRotation = Quaternion.Euler(0, Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg, 0);
-            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * 8f);
+
+            ani.SetFloat("Speed", direction.magnitude);
+            Vector2 movement = CalculateMovementForce(direction, moveSpeed);
+            rb.AddForce(new Vector3(movement.x, 0, movement.y) * Time.deltaTime);
+            //transform.position = transform.position + new Vector3(direction.x,0,direction.y) * Time.deltaTime * moveSpeed;
+            if (direction != Vector2.zero)
+            {
+                Quaternion desiredRotation = Quaternion.Euler(0, Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg, 0);
+                transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * 8f);
+            }
+
+            CheckForObstacles();
         }
-       
+        else
+        {
+            rb.AddForce(new Vector3(direction.x, 0, direction.y) * Time.deltaTime * 5, ForceMode.VelocityChange);
+        }
+
+
+        
+    }
+
+    private void CheckForObstacles()
+    {
+
     }
 
     public void Jump(bool value)
@@ -59,7 +139,9 @@ public class Scr_CharacterMovement : MonoBehaviour
 
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         ani.SetTrigger("Jump");
-        isGrounded = true;
+        jumpResetTimer = jumpResetBuffer;
+        bufferJumpReset = true;
+        SetGrounded(false);
     }
 
     private Vector2 CalculateMovementForce(Vector2 playerInput, float speedMultiplier)
