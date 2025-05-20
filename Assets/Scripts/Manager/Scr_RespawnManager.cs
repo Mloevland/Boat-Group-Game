8c106d77bfcd704056410e69aa47d947ace21195
Mathias2139 <mathias.algot@sybogames.com>
2025-05-20 14:04:15 +0200

    Greybox Updates

--- Assets/Scripts/Manager/Scr_RespawnManager.cs
+++ Assets/Scripts/Manager/Scr_RespawnManager.cs
@@ -6,6 +6,29 @@ public class Scr_RespawnManager : MonoBehaviour
     public GameObjectEvent respawnPlayer;
     private GameObject respawnCheckpoint;
 
+    public float respawnDelay = 1f;
+
+    private bool shouldRespawn;
+    private float respawnTimer;
+
+    private void Update()
+    {
+        if(!shouldRespawn)
+            return;
+
+        respawnTimer += Time.deltaTime;
+        if(respawnTimer >= respawnDelay)
+        {
+            shouldRespawn = false;
+            respawnTimer = 0f;
+            if(respawnCheckpoint != null)
+            {
+                respawnPlayer.Raise(respawnCheckpoint);
+            }
+        }
+        
+    }
+
     public void SetCheckpoint(GameObject gameObject)
     {
         respawnCheckpoint = gameObject;
@@ -13,6 +36,7 @@ public class Scr_RespawnManager : MonoBehaviour
 
     public void RespawnPlayer()
     {
-        respawnPlayer.Raise(respawnCheckpoint);
+        shouldRespawn = true;
+        //respawnPlayer.Raise(respawnCheckpoint);
     }
 }
